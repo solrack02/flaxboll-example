a2e1dce24e60c2f4fba6cba5846d865fc8823e08
Carlos A. <carlosvsk@gmail.com>
2024-09-04 11:22:45 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -108,7 +108,7 @@ stls.height({ pass: { arrayValue: [jsvals.varReader({pass: {
 
             styles:[
         stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "330px"
+          propertieValues: "250px"
         }})] }}), 
         stls.width({ pass: { arrayValue: [jsvals.varReader({pass: {
           path: jsvals.j8({pass: {
