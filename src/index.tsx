f5d858f2f121be3f44364b4e2d2278f29ed364c8
Carlos A. <carlosvsk@gmail.com>
2024-09-06 15:58:10 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -687,14 +687,7 @@ stls.shadows({ pass: {
           (...args:any) => <Elements.DynView pass={{
             elementsProperties:[{}],
 
-            styles:[
-              {
-                backgroundColor: 'white',
-                justifyContent: 'center',
-                minHeight: 22,
-                width: "100%",
-              }
-              ],
+            styles:[[]],
 
             variablePath:[ ],
 
