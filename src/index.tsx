67e4e2420db0cc46a919686fc9f6fe02702fde10
Carlos A. <carlosvsk@gmail.com>
2024-09-06 15:03:31 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -484,139 +484,14 @@ stls.borderRadius({ pass: { arrayValue: [jsvals.j8({pass: {
           propertieValues: "false"
         }})]
         }})],
-            arrItems: [(...args:any) => <Elements.FlatList2 pass={{
-          elementProperties: [
-            
-        (...args:any) => props.horizontal({ pass: {
-          boolean: ['true']
-        }}), (...args:any) => props.ItemSeparatorComponent({ pass: {
-          component: [
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:[{}],
-
-            styles:[stls.width({ pass: { arrayValue: [jsvals.varReader({pass: {
-          path: jsvals.j8({pass: {
-          propertieValues: "all.sizes.fixed.10"
-        }})
-        }})] }})],
-
-            variablePath:[ ],
-
-            expectedVal:[ ],
-
-            childrenItems:[() =><></>],
-
-            args,
-          }}/>
-        ]
-        }})
-          ],
-
-          pData: jsvals.j8({pass: {
-          propertieValues: "all.categs"
-        }}),
-
-          itemElements: [
-            
-
-          (...args:any) => <Elements.DynView pass={{
-            elementsProperties:[{}],
-
-            styles:[
-        
-stls.borderRadius({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "10px"
-        }})] }}), 
-stls.backgroundColor({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "#fff"
-        }})] }}), 
-stls.alignItems({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "center"
-        }})] }}), 
-stls.flexDirection({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "row"
-        }})] }}), 
-stls.shadows({ pass: {
-          shadowColor: ['#000'],
-          shadowOffset: ['{width: 0, height: 0}'],
-          shadowOpacity: ['0.4px'],
-          shadowRadius: ['20px'],
-        }}), 
-stls.marginVertical({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "5px"
-        }})] }}), stls.flex({ pass: { arrayValue: ['1'] }}), 
-        stls.height({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "40px"
-        }})] }}), stls.width({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "90px"
-        }})] }})],
-
-            variablePath:[ ],
-
-            expectedVal:[ ],
-
-            childrenItems:[
-        
-
-    (...args:any) => <Elements.ImageBox pass={{
-      elementsProperties:[{}],
-
-      styles:[
-stls.width({ pass: { arrayValue: [jsvals.varReader({pass: {
-          path: jsvals.j8({pass: {
-          propertieValues: "all.sizes.fixed.30"
-        }})
-        }})] }}), stls.height({ pass: { arrayValue: [jsvals.varReader({pass: {
-          path: jsvals.j8({pass: {
-          propertieValues: "all.sizes.fixed.30"
-        }})
-        }})] }})],
-
-      URIvariablePath:[jsvals.argReader({pass: {
-          argPath: jsvals.j8({pass: {
-          propertieValues: "#0.0.item.image"
-        }}),
-          args
-        }})],
-
-      args,
-    }}/>, (...args:any) => <Elements.Text pass={{
-          arrProps: [
-            {}
-          ],
-
-          arrStyles: [
-            { color: 'black', fontSize: 12, }
-          ],
-
-          children: [
-            jsvals.argReader({pass: {
-          argPath: jsvals.j8({pass: {
-          propertieValues: "#0.0.item.name"
-        }}),
-          args
-        }})
-          ],
-
-          args,
-
-        }}/>],
-
-            args,
-          }}/>
-        
-          ],
-
-          args,
-        }}/>],
+            arrItems: [() => <></>],
             args,
         }}/>],
 
             args,
           }}/>
         , (...args:any) => <Elements.ScrollBar pass={{
-            styles: [stls.flex({ pass: { arrayValue: ['1'] }})],
+            styles: [],
             arrProps: [],
             arrItems: [(...args:any) => <Elements.FlatList2 pass={{
           elementProperties: [
@@ -652,16 +527,12 @@ stls.shadows({ pass: {
           shadowOffset: ['{width: 0, height: 0}'],
           shadowOpacity: ['0.4px'],
           shadowRadius: ['20px'],
-        }}), 
-stls.marginVertical({ pass: { arrayValue: [jsvals.j8({pass: {
+        }}), stls.marginVertical({ pass: { arrayValue: [jsvals.j8({pass: {
           propertieValues: "5px"
-        }})] }}), stls.flex({ pass: { arrayValue: ['1'] }}), 
-        stls.width({ pass: { arrayValue: [jsvals.varReader({pass: {
+        }})] }}), stls.width({ pass: { arrayValue: [jsvals.varReader({pass: {
           path: jsvals.j8({pass: {
           propertieValues: "all.sizes.percent.100"
         }})
-        }})] }}), stls.marginHorizontal({ pass: { arrayValue: [jsvals.j8({pass: {
-          propertieValues: "15px"
         }})] }})],
 
             variablePath:[ ],
