2dfbaac2df9154014f40348d376c0d007d7cea42
Carlos A. <carlosvsk@gmail.com>
2024-09-17 17:14:05 -0300

    Commit generated by Flaxboll.

--- src/tools/base/jsvals/index.ts
+++ src/tools/base/jsvals/index.ts
@@ -77,15 +77,6 @@ type Tprops_j8 = {
 export const j8 = (props: Tprops_j8) => {
   const { propertieValues } = props.pass;
 
-  const isAString = typeof propertieValues === 'string';
-  const check1 = isAString && propertieValues?.includes('px');
-  const check2 = isAString && propertieValues?.includes('%');
-  const checkValues = check1 || check2;
-
-  if (checkValues) {
-    return parseInt(propertieValues.replace(/D/g, ''), 10);
-  }
-
   return propertieValues;
 };
 
