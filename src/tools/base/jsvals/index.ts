4874215bd42c8f5463f8c8b483fd6c2c40b5fb11
Carlos A. <carlosvsk@gmail.com>
2024-09-17 17:06:02 -0300

    Commit generated by Flaxboll.

--- src/tools/base/jsvals/index.ts
+++ src/tools/base/jsvals/index.ts
@@ -77,6 +77,10 @@ type Tprops_j8 = {
 export const j8 = (props: Tprops_j8) => {
   const { propertieValues } = props.pass;
 
+  if (checkValues) {
+    return parseInt(propertieValues.replace(/D/g, ''), 10);
+  }
+
   return propertieValues;
 };
 
