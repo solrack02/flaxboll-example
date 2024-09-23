4f7c47022d8bed6fa8c7e414812e3a08e995137f
Carlos A. <carlosvsk@gmail.com>
2024-09-23 15:24:45 -0300

    Commit generated by Flaxboll.

--- src/tools/base/functions/firebase/fireInit.ts
+++ src/tools/base/functions/firebase/fireInit.ts
@@ -1,6 +1,6 @@
 
 // ---------- set Local Imports
- import FB_APP from 'firebase';
+ import FB_APP from 'firebasejs/9.13.0/firebase-app.js';
 
 type Tprops = { args: any; pass: { fbConfig: any; arrFuncs: any[] } };
 
