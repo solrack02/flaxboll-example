4afe469ef098874d30e77d0414730e72a38f4b78
Carlos A. <carlosvsk@gmail.com>
2024-09-24 16:27:26 -0300

    Commit generated by Flaxboll.

--- src/tools/base/functions/firebase/where.ts
+++ src/tools/base/functions/firebase/where.ts
@@ -4,6 +4,7 @@
 import { initializeApp } from 'firebase/app';
 // import firebase from 'firebase/app';
 // import firestore from 'firebase/firestore';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 // type Tprops = {
 //   args: any;
@@ -23,31 +24,31 @@ import { initializeApp } from 'firebase/app';
 // export const where = async (props: Tprops) => {
 export const where = async () => {
   // console.log('where', firebase, firestore);
-  console.log('where', initializeApp);
+  // console.log('where', initializeApp);
   //   const { initializeApp } = firebase;
   //   const { getFirestore, collection, getDocs } = firestore;
 
   //   console.log('Script 2');
   //   console.log(initializeApp);
   //   // Configurações do Firebase
-  //   const firebaseConfig = {
-  //     apiKey: 'AIzaSyDjAfyEUADq7EaRyFWlGFjP1Eoox9LJHgI',
-  //     authDomain: 'devs-tests-95208.firebaseapp.com',
-  //     projectId: 'devs-tests-95208',
-  //     storageBucket: 'devs-tests-95208.appspot.com',
-  //     messagingSenderId: '750912250366',
-  //     appId: '1:750912250366:web:4629eac789a718a74220af',
-  //   };
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDjAfyEUADq7EaRyFWlGFjP1Eoox9LJHgI',
+    authDomain: 'devs-tests-95208.firebaseapp.com',
+    projectId: 'devs-tests-95208',
+    storageBucket: 'devs-tests-95208.appspot.com',
+    messagingSenderId: '750912250366',
+    appId: '1:750912250366:web:4629eac789a718a74220af',
+  };
 
   //   // Inicializar o Firebase
-  //   const initFb = initializeApp(firebaseConfig);
-  //   console.log({ initFb });
+  const initFb = initializeApp(firebaseConfig);
+  console.log({ initFb });
 
   //   // Inicializar o Firestore
-  //   const db = getFirestore(initFb);
-  //   console.log({ db });
+  const db = getFirestore(initFb);
+  console.log({ db });
 
-  //   const refColl = collection(db, 'users');
-  //   console.log({ refColl });
+  const refColl = collection(db, 'users');
+  console.log({ refColl });
 };
 
