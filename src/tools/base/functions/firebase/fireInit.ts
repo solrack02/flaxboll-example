4523bf793a8180099ffa6b3865e6d60c60ce9bca
Carlos A. <carlosvsk@gmail.com>
2024-09-24 10:27:33 -0300

    Commit generated by Flaxboll.

--- src/tools/base/functions/firebase/fireInit.ts
+++ src/tools/base/functions/firebase/fireInit.ts
@@ -1,6 +1,6 @@
 
 // ---------- set Local Imports
-import { initializeApp, firebase } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 console.log({ initializeApp });
 
 type Tprops = { args: any; pass: { fbConfig: any; arrFuncs: any[] } };
@@ -15,11 +15,9 @@ export const fireInit = async (props: Tprops) => {
   if (typeof fbConfig === 'object') {
     // ---------- set FB Init on a Variable
     const fbInit = initializeApp(fbConfig, 'secondary');
-    const db = firebase.firestore();
     console.log({ fbInit });
-    console.log({ db });
     for (const currFunc of arrFuncs) {
-      await currFunc(db, args);
+      await currFunc(fbInit, args);
     }
   } else {
     console.log('fbConfig precisa ser um objeto', { fbConfig });
